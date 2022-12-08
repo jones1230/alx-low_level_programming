30c0ca84171a5f6ffb8e9c78ba981ba2ab9de1a7
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-08 10:55:31 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,62 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  *
- * Return: Always 0(Success)
+ * Return: 1 if Success.
  */
-int main (void)
+int main(void)
 {
-	int year, month, day;
-	year = 2015;
-	month = 10;
-	day = 19;
-	putchar('a');
-	putchar('n');
-	putchar('d');	
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchar('"');
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar(year);
-	putchar('-');
-	putchar(month);
-	putchar('-');
-	putchar(day);
-	putchar(' ');
-	putchar('\n');
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return(1);
 }
