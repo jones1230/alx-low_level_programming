9ec5a044172f541d3e664773fb1ad8d728a40ede
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-08 10:50:50 -0800

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main (void)
+{
+	int year, month, day;
+	year = 2015;
+	month = 10;
+	day = 19;
+	putchar('a');
+	putchar('n');
+	putchar('d');	
+	putchar(' ');
+	putchar('t');
+	putchar('h');
+	putchar('a');
+	putchar('t');
+	putchar(' ');
+	putchar('o');
+	putchar('f');
+	putchar(' ');
+	putchar('a');
+	putchar('r');
+	putchar('t');
+	putchar(' ');
+	putchar('i');
+	putchar('s');
+	putchar(' ');
+	putchar('u');
+	putchar('s');
+	putchar('e');
+	putchar('f');
+	putchar('u');
+	putchar('l');
+	putchar('"');
+	putchar(' ');
+	putchar('-');
+	putchar(' ');
+	putchar('D');
+	putchar('o');
+	putchar('r');
+	putchar('a');
+	putchar(' ');
+	putchar('K');
+	putchar('o');
+	putchar('r');
+	putchar('p');
+	putchar('a');
+	putchar('r');
+	putchar(',');
+	putchar(' ');
+	putchar(year);
+	putchar('-');
+	putchar(month);
+	putchar('-');
+	putchar(day);
+	putchar(' ');
+	putchar('\n');
+}
