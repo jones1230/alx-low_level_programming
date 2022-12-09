a925a56cecef9b48c9233397d80d2e8d1e4c46b8
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-09 13:51:22 -0800

    Done with task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Prints all alphabets in lower case
+ *
+ *Return: Always 0(Sucess)
+ */
+int main(void)
+{
+	char a = 'a'
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	return (0);
+}
