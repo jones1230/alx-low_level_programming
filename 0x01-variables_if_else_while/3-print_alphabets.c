eb9059845479a2ad014f13226c6a0ceec6ca8883
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-09 14:06:50 -0800

    Task 3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Prints all alphabets in lower case
+ *
+ *Return: Always 0(Sucess)
+ */
+int main(void)
+{
+	char l;
+	char u;
+
+	for (l = 'a'; a <= 'z'; a++)
+	{
+		putchar(l);
+	}
+	for (u = 'A'; a <= 'Z'; A++)
+	{
+		putchar(u);
+	}
+	putchar('\n');
+	return (0);
+}
+
