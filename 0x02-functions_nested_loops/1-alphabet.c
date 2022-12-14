f1dbf3500c5e6fb59374f69a84807e1b1b4502c3
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-13 17:01:03 -0800

    Solved alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet func
+ *
+ * Return: Always 0(Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= z; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
