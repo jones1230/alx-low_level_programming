4d45306d62acdcc2ef7646713d302331d750dd27
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-13 16:35:42 -0800

    Solved putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints string _putchar using a function with same name
+ *
+ * Return: Always 0(Sucess)
+ */
+
+int main(void)
+{
+	char test[7] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(test[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
