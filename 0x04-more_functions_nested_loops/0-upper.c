19362396b4010e3249a0d6606180fced2cf15267
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-15 11:16:16 -0800

    Task 0 dine

---
+++ 0x04-more_functions_nested_loops/0-upper.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper()- Check if letter is in uppercase
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
+int _isupper(int c)
+{
+	int u, l, rv;
+
+	for (u = 65; u <= 96; u++)
+	{
+		if (c == u)
+		{
+			rv = 1;
+			return (rv);
+		}
+	}
+	for (l = 97; l <= 125; l++)
+	{
+		if (c == l)
+		{
+			rv = 0;
+			return (rv);
+		}
+	}
+}
+
