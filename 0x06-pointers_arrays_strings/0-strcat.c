5dbaa3099b8e811b30fd983da6922cfe57f6fd5b
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-21 14:15:39 -0800

    First task done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings
- * @dest - Pointer of First string
- * @src - Pointer of Second string
- * Return - Pointer of dest
+ * _strcat - a functionm that concatenates two strings.
+ * @dest: Pointer of First string
+ * @src: Pointer of Second string
+ * Return: Pointer of dest
  */
 
 char *_strcat(char *dest, char *src);
