1958c3e6e9695ff5b75e59be920d7eb58582d6cb
Jesse Yeboah <jesseyeboah16@gmail.com>
2022-12-21 14:55:37 -0800

    Advanced tasks done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ char *rot13(char *s);
 void print_number(int n);
 void print_buffer(char *b, int size);
 void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
