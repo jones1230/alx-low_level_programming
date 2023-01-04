2e00cdb48d2d02982980cc37ac76423510418820
Jesse Yeboah <jesseyeboah16@gmail.com>
2023-01-03 17:43:46 -0800

    Done with mandatory

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "include.h"
 
 /**
- * _strspn - a function that gets the length of a prefix substring.
+ * _strpbrk - a function that gets the length of a prefix substring.
  * @s: an input string
  * @accept: an input character with to locate into string s
  * Return: returns pointer to c position
