9aa5aa1c8d5bba3a2731e1b2dddb0c4c503e40f3
Jesse Yeboah <jesseyeboah16@gmail.com>
2023-01-09 15:13:36 -0800

    Task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Takes two arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	return (0);
+}
