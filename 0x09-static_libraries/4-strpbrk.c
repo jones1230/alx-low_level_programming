51f6b9b89e9bdc5dea4e8fb8be16d687ef250d35
Jesse Yeboah <jesseyeboah16@gmail.com>
2023-01-11 17:34:55 -0800

    All files in 0x09

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strpbrk - a function that gets the length of a prefix substring.
+ * @s: an input string
+ * @accept: an input character with to locate into string s
+ * Return: returns pointer to c position
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j = 0;
+	int check = 0;
+	char *p = NULL;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			check = 0;
+			if (s[i] == accept[j])
+			{
+				p = (s + i);
+				check = 1;
+				break;
+			}
+		}
+		if (check == 1)
+		{
+			break;
+		}
+	}
+	return (p);
+}
