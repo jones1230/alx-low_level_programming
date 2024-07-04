031e00c9197058493797dc675b1ca8e2aee247e9
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-07-04 13:40:48 -0700

    Removed foreign symbols in file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,11 @@ void rev_string(char *s)
 
 	while (s[index++])
 		len++;
-OAOAOA
-OA	for (index = len - 1; index >= len / 2; index--)
-OA	{
-OAOA		tmp = s[index];
-OA		s[index] = s[len - index - 1];
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
 		s[len - index - 1] = tmp;
-OA	}
-OA}
+	}
+}
